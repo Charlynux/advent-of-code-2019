46ec12dfe3f438454901c3c56153b787f8c72183
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2019-12-01 09:36:38 +0100

    Day 1 - Part 2 in Go naïve implementation

--- day01/day01.go
+++ day01/day01.go
@@ -12,6 +12,14 @@ func fuelRequired(mass int) int {
         return ((mass / 3) - 2)
 }
 
+func fuelRequired2(total int, mass int) int {
+        if (mass <= 0) {
+                return total
+        } else {
+                return fuelRequired2(total + mass, fuelRequired(mass))
+        }
+}
+
 func main() {
         file, err := os.Open("input")
         if err != nil {
@@ -23,6 +31,7 @@ func main() {
         reader := bufio.NewReader(file)
         
         result := 0
+        result2 := 0
         for {
                 line, _, err := reader.ReadLine()
                 
@@ -32,6 +41,8 @@ func main() {
                 
                 value, _ := strconv.Atoi(string(line))
                 result += fuelRequired(value)
+                result2 += fuelRequired2(0, fuelRequired(value));
         }
         fmt.Printf("%d \n", result)
+        fmt.Printf("%d \n", result2)
 }
