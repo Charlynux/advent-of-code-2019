5e21714873b4d3ea49b5ccbf47ead8e631c1403d
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2019-12-02 19:08:22 +0100

    Day 2 - Part 2

--- day02/day02.clj
+++ day02/day02.clj
@@ -24,4 +24,24 @@
       execute
       first))
 
-(solve (read-string (str "[" (slurp "input") "]")))
+(def input (read-string (str "[" (slurp "input") "]")))
+
+(solve input)
+
+(defn solve-generic [noun verb values]
+  (-> values
+      (assoc 1 noun 2 verb)
+      execute
+      first))
+
+(defn find-value [stop-value values]
+  (let [stop? (fn [[_ _ value]] (not= value stop-value))
+        solutions (for [noun (range 0 100) verb (range 0 100)]
+                    [noun verb (solve-generic noun verb input)])]
+    (->> solutions (drop-while stop?) first)))
+
+(defn solve-2 [searched-value values]
+  (let [[noun verb _] (find-value searched-value values)]
+    (+ (* 100 noun) verb)))
+
+(solve-2 19690720 input)
