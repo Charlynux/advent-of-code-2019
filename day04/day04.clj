ac27628b955fd7bf11fe96fde7863f4b47534ce4
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2019-12-04 08:22:30 +0100

    Day 4 - Part 2

--- day04/day04.clj
+++ day04/day04.clj
@@ -6,5 +6,19 @@
                    e (range d 10)
                    f(range e 10)
                    :when (and (or (= a b) (= b c) (= c d) (= d e) (= e f))
-                              (<= 264793 (Integer/parseInt (str a b c d e f))  803935))]
+                              (<= 264793 (Integer/parseInt (str a b c d e f)) 803935))]
+               1)))
+
+(time (count (for [a (range 2 9)
+                   b (range a 10)
+                   c (range b 10)
+                   d (range c 10)
+                   e (range d 10)
+                   f(range e 10)
+                   :when (and (or (and (= a b) (not= b c))
+                                  (and (= b c) (not= a b) (not= c d))
+                                  (and (= c d) (not= b c) (not= d e))
+                                  (and (= d e) (not= c d) (not= e f))
+                                  (and (= e f) (not= d e)))
+                              (<= 264793 (Integer/parseInt (str a b c d e f)) 803935))]
                1)))
