6d041d310ec0dadf0e7a4f2b27f70232f9dfd146
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2019-12-05 22:38:58 +0100

    Day 5 - Part 1 & 2

--- day05/day05.clj
+++ day05/day05.clj
@@ -1,18 +1,60 @@
-(defmulti proceed (fn [instruction & args] instruction))
-(defmethod proceed 1 [_ index values]
-  (let [pos1 (get values (+ index 1))
-        pos2 (get values (+ index 2))
-        pos-result (get values (+ index 3))
-        a (get values pos1)
-        b (get values pos2)]
-    [(+ index 4) (assoc values pos-result (+ a b))]))
-(defmethod proceed 2 [_ index values]
-  (let [pos1 (get values (+ index 1))
-        pos2 (get values (+ index 2))
-        pos-result (get values (+ index 3))
-        a (get values pos1)
-        b (get values pos2)]
-    [(+ index 4) (assoc values pos-result (* a b))]))
+(defn position-mode? [instruction offset]
+  (zero? (mod (quot instruction (Math/pow 10 (inc offset))) 10)))
+
+(defn read-mode [instruction offset]
+  (if (position-mode? instruction offset)
+    (fn [index values] (->> index (+ offset) (get values) (get values)))
+    (fn [index values] (->> index (+ offset) (get values)))))
+
+(comment
+  ((read-mode 1002 1) 0 [1002,4,3,4,33])
+  ((read-mode 1002 2) 0 [1002,4,3,4,33]))
+
+(defn write-mode [instruction offset]
+  (if (position-mode? instruction offset)
+    (fn [index value values] (assoc values (get values (+ index offset)) value))
+    (fn [index value values] (assoc values (+ index offset) value))))
+
+(def ^:dynamic INPUT-VALUE 1)
+
+(def proceed nil)
+(defmulti proceed (fn [instruction & args] (mod instruction 100)))
+(defmethod proceed 1 [instruction index values]
+  (let [a ((read-mode instruction 1) index values)
+        b ((read-mode instruction 2) index values)]
+    [(+ index 4) ((write-mode instruction 3) index (+ a b) values)]))
+(defmethod proceed 2 [instruction index values]
+  (let [a ((read-mode instruction 1) index values)
+        b ((read-mode instruction 2) index values)]
+    [(+ index 4) ((write-mode instruction 3) index (* a b) values)]))
+(defmethod proceed 3 [instruction index values]
+  [(+ index 2) ((write-mode instruction 1) index INPUT-VALUE values)])
+(defmethod proceed 4 [instruction index values]
+  (println ((read-mode instruction 1) index values))
+  [(+ index 2) values])
+(defmethod proceed 5 [instruction index values]
+  (let [next (if (zero? ((read-mode instruction 1) index values))
+               (+ index 3)
+               ((read-mode instruction 2) index values))]
+    [next values]))
+(defmethod proceed 6 [instruction index values]
+  (let [next (if (zero? ((read-mode instruction 1) index values))
+               ((read-mode instruction 2) index values)
+               (+ index 3))]
+    [next values]))
+(defmethod proceed 7 [instruction index values]
+  (let [value (if (< ((read-mode instruction 1) index values)
+                     ((read-mode instruction 2) index values))
+                1
+                0)]
+    [(+ index 4) ((write-mode instruction 3) index value values)]))
+(defmethod proceed 8 [instruction index values]
+  (let [value (if (= ((read-mode instruction 1) index values)
+                     ((read-mode instruction 2) index values))
+                1
+                0)]
+    [(+ index 4) ((write-mode instruction 3) index value values)]))
+
 
 (defn execute [values]
   (loop [index 0 values values]
@@ -22,10 +64,25 @@
         (let [[next-index next-values] (proceed instruction index values)]
           (recur next-index next-values))))))
 
-(def example [1,9,10,3,2,3,11,0,99,30,40,50])
-(assert (= 3500 (first (execute example))))
+(assert (= 3500 (first (execute [1,9,10,3,2,3,11,0,99,30,40,50]))))
+(assert (= 1002 (first (execute [1002,4,3,4,33]))))
+(assert (= 1101 (first (execute [1101,100,-1,4,0]))))
+
+(comment
+  (execute [3,0,4,0,99])
+  (execute [3,9,8,9,10,9,4,9,99,-1,8])
+  (execute [3,9,7,9,10,9,4,9,99,-1,8])
+  (execute [3,3,1108,-1,8,3,4,3,99])
+  (execute [3,3,1107,-1,8,3,4,3,99])
+  (binding [INPUT-VALUE 8]
+    (execute [3,9,8,9,10,9,4,9,99,-1,8])))
 
 (defn read-input [file] (read-string (str "[" (slurp file) "]")))
 
 (assert (= 3654868 (-> (read-input "day02/input") (assoc 1 12 2 2) execute first)))
 (assert (= 19690720 (-> (read-input "day02/input") (assoc 1 70 2 14) execute first)))
+
+(-> (read-input "day05/input") execute)
+
+(binding [INPUT-VALUE 5]
+  (-> (read-input "day05/input") execute))
