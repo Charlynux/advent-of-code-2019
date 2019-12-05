e74fbd3395e3068c83765e03707f02005826492e
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2019-12-05 21:16:22 +0100

    Day 5 - Fix refactor

--- day05/day05.clj
+++ day05/day05.clj
@@ -1,15 +1,15 @@
 (defmulti proceed (fn [instruction & args] instruction))
 (defmethod proceed 1 [_ index values]
-  (let [pos1 (+ index 1)
-        pos2 (+ index 2)
-        pos-result (+ index 3)
+  (let [pos1 (get values (+ index 1))
+        pos2 (get values (+ index 2))
+        pos-result (get values (+ index 3))
         a (get values pos1)
         b (get values pos2)]
     [(+ index 4) (assoc values pos-result (+ a b))]))
 (defmethod proceed 2 [_ index values]
-  (let [pos1 (+ index 1)
-        pos2 (+ index 2)
-        pos-result (+ index 3)
+  (let [pos1 (get values (+ index 1))
+        pos2 (get values (+ index 2))
+        pos-result (get values (+ index 3))
         a (get values pos1)
         b (get values pos2)]
     [(+ index 4) (assoc values pos-result (* a b))]))
@@ -21,6 +21,9 @@
         values
         (let [[next-index next-values] (proceed instruction index values)]
           (recur next-index next-values))))))
+
+(def example [1,9,10,3,2,3,11,0,99,30,40,50])
+(assert (= 3500 (first (execute example))))
 
 (defn read-input [file] (read-string (str "[" (slurp file) "]")))
 
