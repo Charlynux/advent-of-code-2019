c7eef5b8e7a62e36a9f3e14ad4fa56c46c9b0b6a
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2019-12-10 15:40:21 +0100

    Day 10 - Trigonometric calculations

--- day10/day10.clj
+++ day10/day10.clj
@@ -16,3 +16,21 @@
 #####
 ....#
 ...##"))
+
+(let [a (Math/toRadians 90)] [(Math/cos a) (Math/sin a)])
+
+(Math/asin 0)
+(Math/acos 0)
+
+(defn square [n] (* n n))
+
+(defn calc [[Ax Ay] [Cx Cy]]
+  (Math/toDegrees
+   (let [[Bx By] [(inc Ax) Ay]
+         a (Math/sqrt  (+ (square (- Bx Cx)) (square (- By Cy))))
+         b (Math/sqrt  (+ (square (- Ax Cx)) (square (- Ay Cy))))
+         c (Math/sqrt  (+ (square (- Bx Ax)) (square (- By Ay))))]
+     (Math/acos (/ (- (+ (square b) (square c)) (square a)) (* 2 b c))))))
+
+(calc [0 0] [0 1])
+(calc [0 0] [0 -1])
