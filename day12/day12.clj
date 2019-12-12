57afa2c991e7cc85e059edf81d32b4c791306294
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2019-12-12 12:39:47 +0100

    Day 12 - Part 1

---
+++ day12/day12.clj
@@ -0,0 +1,67 @@
+(def test-moons
+  [[-1 0 2]
+   [2 -10 -7]
+   [4 -8 8]
+   [3 5 -1]])
+
+(def moons
+  [[8 0 8]
+   [0 -5 -10]
+   [16 10 -5]
+   [19 -10 -7]])
+
+(defn init-moons [moons]
+  (map (fn [moon] (hash-map :position moon :velocity [0 0 0])) moons))
+
+(defn update-val [a b] (cond (= a b) 0
+                             (< a b) +1
+                             (> a b) -1))
+
+(defn update-velocity [moons]
+  (map
+   (fn [moon]
+     (let [updates (for [other moons
+                         :when (not= other moon)]
+                     (mapv update-val (:position moon) (:position other)))]
+       (update moon :velocity (fn [v] (reduce #(mapv + %1 %2) v updates)))))
+   moons))
+
+(comment
+  (update-velocity
+   (init-moons test-moons)))
+
+(defn move-moon [moon]
+  (update moon :position (fn [p] (mapv + p (:velocity moon)))))
+
+(comment
+  (move-moon {:position [-1 0 2] :velocity [3 -1 -1]}))
+
+(defn iteration [moons]
+  (->> moons
+       update-velocity
+       (map move-moon)))
+
+(comment
+  (take 10 (iterate iteration (init-moons test-moons)))
+
+  (nth (iterate iteration (init-moons test-moons)) 10))
+
+(defn potential-energy [moon] (reduce + (map #(Math/abs %) (:position moon))))
+(defn kinetic-energy [moon] (reduce + (map #(Math/abs %) (:velocity moon))))
+
+(defn total-energy [moon] (* (potential-energy moon) (kinetic-energy moon)))
+
+(comment
+  (map total-energy
+       (nth (iterate iteration (init-moons test-moons)) 10))
+  )
+
+(defn solve [moons]
+  (->> moons
+       init-moons
+       (iterate iteration)
+       (#(nth % 1000))
+       (map total-energy)
+       (reduce + 0)))
+
+(solve moons)
