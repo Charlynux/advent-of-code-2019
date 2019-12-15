1c0d1d55dba768386932798362532249f94cc454
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2019-12-15 20:38:53 +0100

    Day 15 - Part 1

--- day15/day15.clj
+++ day15/day15.clj
@@ -15,7 +15,7 @@
 (def ->movement
   {NORTH [0 1]
    SOUTH [0 -1]
-   EAST [-1 0]
+   EAST [1 0]
    WEST [-1 0]})
 
 (defn apply-movement [position movement]
@@ -33,21 +33,63 @@
       (assoc :inputs [movement] :outputs [])
       intcode/run))
 
-(defn chose-move [state] NORTH)
+;; CURRENT STATE
+;; MOVEMENTS => FILTER DESTINATION OPEN => MAP STATE MOVEMENT => PROGRAM-UPDATE (STATUS) => BOARD-UPDATE POSITION-UPDATE
 
-(defn move-state [{:keys [program position] :as state} movement]
-  (let [destination (apply-movement position movement)
-        updated (move program movement)
-        status (get-in updated [:outputs 0] ERROR)]
-    (when (= status ERROR)
-      (throw (ex-info "No output" updated)))
-    (-> state
-        (update :board #(assoc % destination status))
-        (assoc :program updated
-               :position (if (= status HIT_WALL) position destination)))))
+;; Choose best open
+;; PUT in closed
+;; move prog movement -> updated-prog
+;; update board and position
+;; STATUS HIT_WALL = RECUR
+;; STATUS FOUND OX = STOP
+;; STATUS OK...
+#_(->> (keys ->movement)
+       (map #(apply-movement position %))
+       (remove closed?)) ;; Remove closed and better open
+;; Merge with open
+;; RECUR.
 
-;;  { :area { [1, 0] => WALL } :position [0, 0] }
-(let [state {:program (intcode/init-program (intcode/read-input-file "day15/input"))
-             :position [0 0]
-             :board {}}]
-  (move state (chose-move state)))
+(defn iteration [state]
+  (let [node (first (sort-by :priority (vals (:open state))))
+        result (move (:program node) (:movement node))
+        status (get-in result [:outputs 0] ERROR)
+        new-state (-> state
+                      (assoc :path-length (:priority node))
+                      (update :closed conj (:target node))
+                      (update :open dissoc (:target node))
+                      (update :board assoc (:target node) status))]
+    (condp = status
+      FOUND new-state
+      HIT_WALL (recur new-state)
+      OK (recur (update new-state :open #(merge % (open-neighbors state result node))))
+      (throw (ex-info "No output" {:state new-state :program result})))))
+
+(defn closed? [state node]
+  (or ((:closed state) (:target node))
+      (< (get-in state [:open (:target node) :priority] Integer/MAX_VALUE)
+         (:priority node))))
+
+(defn open-neighbors [state program {:keys [target priority]}]
+  (let [create-open (fn [movement]
+                      {:program program
+                       :movement movement
+                       :target (apply-movement target movement)
+                       :priority (inc priority)})]
+    (into {}
+          (comp
+           (map create-open)
+           (remove #(closed? state %))
+           (map (juxt :target identity)))
+          (keys ->movement))))
+
+(= 4 (count (open-neighbors { :closed #{} :open {} } ["not", "a", "program"] {:target [0 0] :priority 0})))
+(= 3 (count (open-neighbors { :closed #{[0 1]} :open {} } ["not", "a", "program"] {:target [0 0] :priority 0})))
+(= 3 (count (open-neighbors { :closed #{} :open {[1 0] { :priority 0 }} } ["not", "a", "program"] {:target [0 0] :priority 0})))
+
+(time (let [init-state {:open (open-neighbors { :closed #{} }
+                                              (intcode/init-program (intcode/read-input-file "day15/input"))
+                                              {:target [0 0] :priority 0})
+                        :board {}
+                        :priority 0
+                        :closed #{[0 0]}}]
+        (:path-length (iteration init-state))))
