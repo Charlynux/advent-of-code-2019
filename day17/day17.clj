caf60ab542a6d2386f6c65083578406779466bed
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2019-12-17 12:40:34 +0100

    Day 17 - Part 1

--- day17/day17.clj
+++ day17/day17.clj
@@ -12,5 +12,18 @@
   (map-indexed (fn [x v]
                  [[x y] v]) line))
 
-(def indexed-view
-  (into {} (comp (map-indexed index-line) cat) camera-view))
+(def grid->coords (comp (map-indexed index-line) (mapcat identity)))
+
+(def indexed-view (into {} grid->coords camera-view))
+(def scaffolds (into [] (comp grid->coords (filter (comp #{\#} second)) (map first)) camera-view))
+
+(defn neighbors [position]
+  (map #(mapv + position %) [[0 1] [0 -1] [1 0] [-1 0]]))
+
+(def count-scaffolds-neighbors
+  (comp count #(filter #{\#} %) #(map indexed-view %) neighbors))
+
+(->> scaffolds
+     (filter #(= 4 (count-scaffolds-neighbors %)))
+     (map #(apply * %))
+     (reduce +))
